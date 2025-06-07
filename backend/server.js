4a1ac099d588d12e8e95094272ab3cd4a2c54a4e
Yawin <wyawin@gmail.com>
2025-06-07 15:55:14 +0700

    update port

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors());
